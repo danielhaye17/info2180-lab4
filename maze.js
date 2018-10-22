0719bf1c5c2962deb93a67b2652a47d007e7871c
danielhaye17 <danielhaye17@gmail.com>
2018-10-22 17:31:53 -0500

    Exercise 5

--- maze.js
+++ maze.js
@@ -1,43 +1,48 @@
+
 let lose = false;
 let commence = false;
 window.onload = function(){
-    /* Exercise 1 
-    
-        let wall_Colour = document.getElementById("boundary1");
-	    wall_Colour.addEventListener("mouseover", function() {
-            this.setAttribute("class","boundary youlose");	
-        });
-    */
-      
-    // Exercise 2
-    
     let wall_Colour = document.querySelectorAll(".boundary");
-    let start_game = document.getElementById("start");
-    start_game.addEventListener("mouseover",function(){
-    for(m = 0; m <wall_Colour.length-1;m++){  
-        wall_Colour[m].addEventListener("mouseover",function() {
-            for(m = 0; m < wall_Colour.length-1;m++){
-                wall_Colour[m].setAttribute("class", "boundary youlose");  
-                   
-            }
-            alert("You lose");  
-        });
+    for (let m = 0; m < wall_Colour.length-1; m++) {
+        wall_Colour[m].addEventListener("mouseover", end_game);
     }
-    });
+    document.getElementById("end").addEventListener("mouseover",game_result);
+    document.getElementById("start").addEventListener("click",start_game);
+}
 
+function end_game(){
+    if(commence){
+        setBoundaryCSS("boundary youlose");
+        lose = true;
+    }
+}
 
- // Exercise 3  
-        end = document.getElementById("end");
-        end.addEventListener("mouseover", function(){
-        alert("You win");
-        });
+function game_result(){
+    if(commence){
+        if(!lose){
+            setStat("You Win!");
+        }
+        else{
+            setStat("You Lose!");
+        }
+        commence = false;
+    }
+}
 
- // Exercise 4
-restart = document.getElementById("start");
-restart.addEventListener("click", function() {
-    location.reload();
-});
- 
- // Exercise 5 
+function start_game(){
+    setBoundaryCSS("boundary");
+    commence = true;
+    lose = false;
+    setStat("Game In Session");
+}
 
+function setBoundaryCSS(cssName){
+    var wall_Colour = document.querySelectorAll(".boundary");
+    for (let m = 0; m < wall_Colour.length-1; m++) {
+        wall_Colour[m].className = cssName;
+    }
+}
+
+function setStat(stat){
+    document.getElementById("status").innerHTML = stat;
 }
