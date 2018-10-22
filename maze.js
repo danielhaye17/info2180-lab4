6ab525cde7646a8015575f0e9c3ae4e78ef68821
danielhaye17 <danielhaye17@gmail.com>
2018-10-22 17:02:25 -0500

    Exercise 4

--- maze.js
+++ maze.js
@@ -1,5 +1,5 @@
 let lose = false;
-let win = false;
+let commence = false;
 window.onload = function(){
     /* Exercise 1 
     
@@ -12,21 +12,32 @@ window.onload = function(){
     // Exercise 2
     
     let wall_Colour = document.querySelectorAll(".boundary");
+    let start_game = document.getElementById("start");
+    start_game.addEventListener("mouseover",function(){
     for(m = 0; m <wall_Colour.length-1;m++){  
         wall_Colour[m].addEventListener("mouseover",function() {
             for(m = 0; m < wall_Colour.length-1;m++){
-                wall_Colour[m].setAttribute("class", "boundary youlose"); 
-                lose = true;      
-            } 
+                wall_Colour[m].setAttribute("class", "boundary youlose");  
+                   
+            }
+            alert("You lose");  
         });
     }
-    if (lose === true) {
-        alert("You lose"); 
-    }
-    
+    });
 
-    
-     
+
+ // Exercise 3  
+        end = document.getElementById("end");
+        end.addEventListener("mouseover", function(){
+        alert("You win");
+        });
+
+ // Exercise 4
+restart = document.getElementById("start");
+restart.addEventListener("click", function() {
+    location.reload();
+});
  
-}
+ // Exercise 5 
 
+}
