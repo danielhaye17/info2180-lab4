4db2615de6820283ad19289d1fc406ea5e8f58b8
danielhaye17 <danielhaye17@gmail.com>
2018-10-22 10:59:31 -0500

    Exercise 1

---
+++ maze.js
@@ -0,0 +1,14 @@
+
+window.onload = function(){
+    /* Exercise 1 */
+    
+        let wall_Colour = document.getElementById("boundary1");
+	    wall_Colour.addEventListener("mouseover", function() {
+            this.setAttribute("class","boundary youlose");	
+        });
+     
+    
+     
+ 
+}
+
